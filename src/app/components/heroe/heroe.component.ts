564f9df4e548cdb41641883195e19c5c312b931e
lelalexi <alexisgarcia406@gmail.com>
2018-10-07 00:50:47 -0300

    Agregado pagina descripcion de heroe y uso de *ngIf

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { HeroesService } from '../Services/heroes.service';
+
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
 })
 export class HeroeComponent {
 
-  constructor( private activatedRoute: ActivatedRoute) {
+  heroe: any = {};
+
+  constructor( private activatedRoute: ActivatedRoute,
+                private _heroesService: HeroesService) {
     this.activatedRoute.params.subscribe( params => {
-      console.log( params['id'] ); // ESE 'ID' ES EL MISMO QUE DEFINIMOS EN EL ARCHIVOS DE RUTA (MIRALO SI NO ESTAS SEGURO)
+      this.heroe = this._heroesService.getHeroe( params['id']);
+      // ESE 'ID' ES EL MISMO QUE DEFINIMOS EN EL ARCHIVOS DE RUTA (MIRALO SI NO ESTAS SEGURO)
     } );
   }
 
