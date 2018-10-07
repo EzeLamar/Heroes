f6fb7acf63c02b6c2f302edd9ef80a709a995ff8
lelalexi <alexisgarcia406@gmail.com>
2018-10-07 01:21:59 -0300

    Un poco de pipes...

--- src/app/components/Services/heroes.service.ts
+++ src/app/components/Services/heroes.service.ts
@@ -66,6 +66,18 @@ export class HeroesService {
     getHeroe( ind: string ): Heroe {
         return this.heroes[ind];
     }
+    buscarHeroes( termino: string) {
+      let heroesArr: Heroe[] = [];
+      termino = termino.toLowerCase();
+
+      for ( let heroe of this.heroes ){
+        let nombre = heroe.nombre.toLowerCase();
+        if ( nombre.indexOf( termino ) >= 0) {
+          heroesArr.push( heroe );
+        }
+      }
+      return heroesArr;
+    }
 }
 
 export interface Heroe {
